d375de0a4b77a2315511026dca81ab2a72a33532
Karl <enriquezkharl14@gmail.com>
2022-10-18 22:14:41 +0800

    navigation animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./sass/main.scss";
+
+const header = document.querySelector(".header");
+
+const fixNav = () => {
+   if (window.scrollY > header.offsetHeight + 900) header.classList.add("active");
+   else header.classList.remove("active");
+};
+
+window.addEventListener("scroll", fixNav);

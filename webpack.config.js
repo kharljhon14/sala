5224b7c3b7e50fd3067cbdc310b00ce3f6e69acb
Karl <enriquezkharl14@gmail.com>
2022-10-18 22:05:05 +0800

    layouts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
    entry: path.resolve(__dirname, "./src/index.js"),
@@ -10,9 +11,13 @@ module.exports = {
       clean: true,
    },
    devServer: {
+      static: {
+         directory: path.resolve(__dirname, "dist"),
+      },
       port: 3000,
       hot: true,
       open: true,
+      historyApiFallback: true,
    },
    plugins: [
       new HTMLWebpackPlugin({
@@ -22,21 +27,21 @@ module.exports = {
          template: "./src/index.html",
          filename: "index.html",
       }),
+      new MiniCssExtractPlugin(),
    ],
    module: {
       rules: [
          {
             test: /\.css$/,
-            use: ["style-loader", "css-loader"],
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
          },
          {
             test: /\.s[ca]ss$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
          },
          {
             test: /.(jpg|png|jpeg|gif)$/,
             type: "asset/resource",
-                
          },
       ],
    },
